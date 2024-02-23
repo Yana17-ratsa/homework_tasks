edcb2301241e19135f6338ca588fc6cac2b1341c
Yana17-ratsa <ratsayana13@gmail.com>
2024-02-23 12:41:34 +0200

    task 4 (from 20.02. lection)

---
+++ js/task-4.js
@@ -0,0 +1,25 @@
+// З 22.02.2024. Модуль 7 Заняття 2
+
+/**
+ * Подія сlick
+ *
+ * Натискаючи на кнопку "Click me" змусь
+ * червоний квадратик зміщуватись на 50px по діагоналі
+ */
+
+
+const clickMe = document.querySelector(".js-click");
+const box = document.querySelector(".js-box");
+
+clickMe.addEventListener("click", handleClick);
+
+let step = 0;
+
+function handleClick() {
+    console.log("Квадрат переміщується на 50 пікселів донизу і на 50 пікселів праворуч");
+    step += 50;
+
+    box.style.marginTop = `${step}px`;
+    box.style.marginLeft = `${step}px`;
+    
+}
